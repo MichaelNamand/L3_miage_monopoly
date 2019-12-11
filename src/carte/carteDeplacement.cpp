3251dfa8df6edcb22a924074500e188aa91c7082
AmB974 <ambriii.mussard@gmail.com>
2019-12-11 12:22:59 +0100

    added cpp

--- src/carte/carteDeplacement.cpp
+++ src/carte/carteDeplacement.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "carteDeplacement.h"
+
+
+carteDeplacement::carteDeplacement(const string &texte, int cases):
+        carte{texte}, d_cases{cases} {}
+
+void carteDeplacement::action(joueur j) {
+    int tmp=j.getIndexCase();
+    if(tmp<d_cases) {
+        j.seDeplacer(d_cases - tmp);
+    }
+    else{
+        int case_inter;
+        j.seDeplacer(40-tmp+(d_cases+1));
+    }
+
+}
