e5a7f5e22fcb63113b3df5cbf9268bfc918e591a
Michael <mjs.namand@gmail.com>
2019-12-11 12:02:26 +0100

    ADD propriete + rue implementations

--- src/case/propriete/propriete.cpp
+++ src/case/propriete/propriete.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "propriete.h"
+
+propriete::propriete(int valeurHypotheque, int prix): d_prix{prix}, d_valeurHypotheque{valeurHypotheque} {}
+
+int propriete::getValeurHypotheque() const {
+    return d_valeurHypotheque;
+}
+
+int propriete::getPrix() const {
+    return d_prix;
+}
+
+joueur *propriete::getJoueur() const {
+    return d_joueur;
+}
--- src/case/propriete/rue.cpp
+++ src/case/propriete/rue.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "rue.h"
+
+int rue::getNbMaisons() const {
+    return d_nbMaisons;
+}
+
+int rue::getNbHotels() const {
+    return d_nbHotels;
+}
