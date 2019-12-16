edd8791729291c783e9b6fa67d03f87e84bb9b6f
AmB974 <ambriii.mussard@gmail.com>
2019-12-16 11:41:29 +0100

    ADD class carteAllerVers + class implementations

--- src/carte/carteDeplacement.cpp
+++ src/carte/carteDeplacement.cpp
@@ -5,17 +5,11 @@
 #include "carteDeplacement.h"
 
 
-carteDeplacement::carteDeplacement(const string &texte, int cases):
+carteDeplacement::carteDeplacement(const string &texte, int cases) :
         carte{texte}, d_cases{cases} {}
 
 void carteDeplacement::action(joueur j) {
-    int tmp=j.getIndexCase();
-    if(tmp<d_cases) {
-        j.seDeplacer(d_cases - tmp);
-    }
-    else{
-        int case_inter;
-        j.seDeplacer(40-tmp+(d_cases+1));
-    }
+
+    j.seDeplacer(d_cases);
 
 }
