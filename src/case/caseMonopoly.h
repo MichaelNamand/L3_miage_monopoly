beaed534a7627d80903ef3f9a7bca3f26625619b
Louay <louay.kass@gmail.com>
2019-12-16 22:23:45 +0100

    FIX Git conflict casMonopoly.h

--- src/case/caseMonopoly.h
+++ src/case/caseMonopoly.h
@@ -5,18 +5,14 @@
 #ifndef L3_MIAGE_MONOPOLY_CASEMONOPOLY_H
 #define L3_MIAGE_MONOPOLY_CASEMONOPOLY_H
 
-<<<<<<< HEAD
 #include <string>
 
-=======
->>>>>>> master
 #define DT_PIOCHE 1
 #define DT_PROPRIETE 2
 #define DT_FLUX 3
 #define DT_ALLERPRISON 4
 #define DT_ATTENTE 5
 
-<<<<<<< HEAD
 using std::string;
 
 class caseMonopoly {
@@ -31,17 +27,6 @@ public :
 private :
     const string d_nom;
     const int d_type;
-=======
-class caseMonopoly {
-public :
-    virtual ~caseMonopoly();
-
-    virtual void action() const = 0;
-
-private :
-    string d_nom;
-    int d_type;
->>>>>>> master
 };
 
 
