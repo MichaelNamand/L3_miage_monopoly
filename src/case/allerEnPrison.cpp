b81620134e3de6b29ffa11fe6b65e7c106d3dfd2
Louay <louay.kass@gmail.com>
2019-12-16 22:17:21 +0100

    ADD class implementations

--- src/case/allerEnPrison.cpp
+++ src/case/allerEnPrison.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "allerEnPrison.h"
+
+#include <string>
+
+using std::string;
+
+allerEnPrison::allerEnPrison(const string& nom, int type): caseMonopoly{nom,type}
+{}
+
+allerEnPrison::~allerEnPrison() {}
+
+void allerEnPrison::action(joueur joueur) {
+    joueur.mettreEnPrison() = true;
+    joueur.seDeplacer(-20);  //de la case AllezPrison à la case EnPrison/VisitePrison
+                                     //nombre négatif pour ne pas gagner 200 euros de CaseDepart
+}
+
--- src/case/pioche.h
+++ src/case/pioche.h
@@ -6,8 +6,19 @@
 #define L3_MIAGE_MONOPOLY_PIOCHE_H
 
 
-class pioche {
+#include "caseMonopoly.h"
+#include "../carte/carte.h"
 
+class pioche : public caseMonopoly{
+public:
+    pioche();
+
+    virtual ~pioche();
+
+    virtual void action() override;   //pioche une carte et affiche le contenu
+
+private :
+    vector <vector<carte*>> d_pioches;
 };
 
 
