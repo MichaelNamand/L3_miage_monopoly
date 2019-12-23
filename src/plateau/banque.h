d561dd1da6a2bdba783263dd9a5083036897a283
Till <till.rouvier@gmail.com>
2019-12-23 20:33:35 +0100

    ADDED banque & prison

---
+++ src/plateau/banque.h
@@ -0,0 +1,14 @@
+//
+// Created by terminn on 12/23/19.
+//
+
+#ifndef L3_MIAGE_MONOPOLY_BANQUE_H
+#define L3_MIAGE_MONOPOLY_BANQUE_H
+
+
+class banque {
+
+};
+
+
+#endif //L3_MIAGE_MONOPOLY_BANQUE_H
