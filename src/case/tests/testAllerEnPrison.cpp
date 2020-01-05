9487250dc3333a8f6fff5b5a307cb1397191089d
Louay <louay.kass@gmail.com>
2020-01-05 18:54:31 +0100

    FIXED test allerEnPrison & fluxMonetaire

--- src/case/tests/testAllerEnPrison.cpp
+++ src/case/tests/testAllerEnPrison.cpp
@@ -1,7 +1,7 @@
 //
 // Created by DELL on 22/12/2019.
 //
-#include "doctest.h"
+#include "../../../includes/doctest.h"
 #include "../allerEnPrison.h"
 #include "../../joueur/joueur.h"
 
@@ -10,6 +10,6 @@ TEST_CASE("Test sur l'action aller en prison")
     joueur j{"testeur"};
     allerEnPrison c{"Aller en prison",DT_ALLERPRISON};
     c.action(j);
-    ASSERT_EQ(20,j.getIndexCase());
+    REQUIRE_EQ(20,j.getIndexCase());
 }
 
