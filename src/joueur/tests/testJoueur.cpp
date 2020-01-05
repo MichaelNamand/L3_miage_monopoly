49215cd4651ebc0e9d7a679f9228749f36a71f52
Till <till.rouvier@gmail.com>
2020-01-05 16:06:06 +0100

    ADD test joueur

--- src/joueur/tests/testJoueur.cpp
+++ src/joueur/tests/testJoueur.cpp
@@ -2,3 +2,28 @@
 // Created by terminn on 1/5/20.
 //
 
+TEST_CASE("Tests sur les méthodes liees au joueur") {
+    joueur j{"Testeur"};
+    SUB_CASE("Le résultat des des doivent etre entre 1 et 6 chacun") {
+        vector<int> des = j.lancerDes();
+        REQUIRE_GE(1,des[0]);
+        REQUIRE_GE(1,des[1]);
+        REQUIRE_LE(6,des[0]);
+        REQUIRE_LE(6,des[1]);
+    }
+    SUB_CASE("Si le joueur gagne un salaire son argent augmente de 200") {
+        int argentAvant = j.getArgent();
+        j.gagnerSalaire();
+        REQUIRE_EQ(argentAvant+200,j.getArgent());
+    }
+    SUB_CASE("Deplacer vers une case") {
+        int argentAvant = j.getArgent();
+        j.deplacerA(20,true);
+        REQUIRE_EQ(argentAvant,j.getArgent());
+        REQUIRE_EQ(20,j.getIndexCase());
+
+        j.deplacerA(10,true);
+        REQUIRE_EQ(argentAvant+200,j.getArgent());
+        REQUIRE_EQ(10,j.getIndexCase());
+    }
+}
--- src/plateau/tests/testPrison.cpp
+++ src/plateau/tests/testPrison.cpp
@@ -6,23 +6,23 @@
 #include "../prison.h"
 #include "../../joueur/joueur.h"
 
-TEST_CASE("Tests sur les méthodes liées à la prison") {
+TEST_CASE("Tests sur les méthodes liees à la prison") {
     joueur j{"Testeur"};
     prison p{};
-    SUB_CASE("Si le joueur sort de prison, il sera sur la 10ème case") {
+    SUB_CASE("Si le joueur sort de prison, il sera sur la 10eme case") {
         p.sortirDePrison(j);
-        ASSERT_EQ(10,j.getIndexCase());
+        REQUIRE_EQ(10,j.getIndexCase());
     }
     SUB_CASE("Le nombre de jours en prison du joueur doit incrémenter") {
         int nbJoursAvant = j.getTourEnPrison();
         p.unJourDePlus(j);
-        ASSERT_EQ(nbJoursAvant+1,j.getTourEnPrison());
+        REQUIRE_EQ(nbJoursAvant+1,j.getTourEnPrison());
     }
-    SUB_CASE("Si le joueur sort de prison en payant, son argent total est réduit de 50 et il sera sur la 10ème case") {
+    SUB_CASE("Si le joueur sort de prison en payant, son argent total est reduit de 50 et il sera sur la 10eme case") {
         int argentAvant = j.getArgent();
         p.payerEnSortant(j);
-        ASSERT_EQ(argentAvant-50,j.getArgent());
-        ASSERT_EQ(10,j.getIndexCase());
+        REQUIRE_EQ(argentAvant-50,j.getArgent());
+        REQUIRE_EQ(10,j.getIndexCase());
     }
 }
 
